b34810ed59a7ab7b569f86f3f57bfb9d9e33cbeb
aatamanirbhar <95468485+aatamanirbhar@users.noreply.github.com>
2026-04-15 06:47:45 +0000

    redeploy

--- app/coupon/page.jsx
+++ app/coupon/page.jsx
@@ -1,7 +1,9 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 export default function DataPage() {
   const [form, setForm] = useState({
